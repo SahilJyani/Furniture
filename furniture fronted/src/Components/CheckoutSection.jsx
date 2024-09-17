56c5f6ad2640c3679f30902b229f7fd497d47f0d
SahilJyani <161322819+SahilJyani@users.noreply.github.com>
2024-09-17 23:19:21 +0530

    Update CheckoutSection.jsx

--- furniture fronted/src/Components/CheckoutSection.jsx
+++ furniture fronted/src/Components/CheckoutSection.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { json, Link, useNavigate } from "react-router-dom";
 
 function CheckoutSection() {
-  let [formdata,setformdata]= useState({country:"India",firstname:"manish",lastname:"solanki",companyName:"XYZ",address:"jaipur",state:"Rajasthan",Zip:"302028",email:"manish@gmail.com",phoneNo:"9878459898",Notes:"helo every one"})
+  let [formdata,setformdata]= useState({country:"India",firstname:"SAHIL",lastname:"JYANI",companyName:"XYZ",address:"jaipur",state:"Rajasthan",Zip:"302028",email:"sahiljyani81@gmail.com",phoneNo:"9992633675",Notes:"helo every one"})
   let nevigate=useNavigate()
 
   let[cartdata,setcartdata]=useState(JSON.parse(localStorage.getItem('cartData'))||[])
